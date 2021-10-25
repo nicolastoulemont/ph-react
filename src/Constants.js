94dce43739c2a41bcfeaab5a693a45d6a2e2019b
EliotAndres <EliotAndres@users.noreply.github.com>
2021-10-25 22:32:14 +0200

    Simplify docs.

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://sdk.photoroom.com/v1/segment'
 export const BASE64_IMAGE_HEADER = 'data:image/png;base64,'
-export const API_KEY = process.env.REACT_APP_API_KEY || 'SET REACT_APP_API_URL environment variable to your API key'
+export const API_KEY = process.env.REACT_APP_API_KEY || 'SET REACT_APP_API_KEY environment variable to your API key'
